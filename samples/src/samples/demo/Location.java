df01f7429d106bfdb0e3a97588c9ec683536864c
Kirrtiyaini <kirrti03skk@gmail.com>
2022-03-10 15:18:07 +0800

    added more attributes

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -4,6 +4,9 @@ package samples.demo;
 public class Location {
 	
 	private String name;
+	private int locationID;
+	private int description;
+	private int age;
 
 	public String getName() {
 		return name;
@@ -11,6 +14,30 @@ public class Location {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getLocationID() {
+		return locationID;
+	}
+
+	public void setLocationID(int locationID) {
+		this.locationID = locationID;
+	}
+
+	public int getDescription() {
+		return description;
+	}
+
+	public void setDescription(int description) {
+		this.description = description;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 }
